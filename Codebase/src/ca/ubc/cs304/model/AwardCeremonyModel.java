fdfbaab5cfc31e9d0b77713759cad4d9a5d080a1
aalchen <43652006+aalchen@users.noreply.github.com>
2023-04-01 22:13:55 -0700

    adding back er diagram models

---
+++ Codebase/src/ca/ubc/cs304/model/AwardCeremonyModel.java
@@ -0,0 +1,28 @@
+package ca.ubc.cs304.model;
+
+import java.util.Date;
+
+public class AwardCeremonyModel {
+
+	private final int viewer_count;
+	private final Date date;
+	private final String venue_name;
+	
+	public AwardCeremonyModel(int viewer_count, Date date, String venue_name) {
+		this.viewer_count = viewer_count;
+		this.date = date;
+		this.venue_name = venue_name;
+	}
+
+	public int getViewerCount() {
+		return viewer_count;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getVenueName() {
+		return venue_name;
+	}
+}
---
+++ Codebase/src/ca/ubc/cs304/model/SponsorsModel.java
@@ -0,0 +1,28 @@
+package ca.ubc.cs304.model;
+
+import java.util.Date;
+
+public class SponsorsModel {
+
+	private final String company_name;
+	private final Date awardceremony_date;
+	private final int money;
+	
+	public SponsorsModel(String company_name, Date awardceremony_date, int money) {
+		this.company_name = company_name;
+		this.awardceremony_date = awardceremony_date;
+		this.money = money;
+	}
+
+	public String getCompanyName() {
+		return company_name;
+	}
+
+	public Date getAwardCeremonyDate() {
+		return awardceremony_date;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+}
